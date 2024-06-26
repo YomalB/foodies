1ae8c86e1af7bb8b1482c50b9071724f6a529cb9
YomalB <bashiyml10smd@gmail.com>
2024-06-27 02:08:22 +0530

    update

--- lib/admin/add_food.dart
+++ lib/admin/add_food.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:foodies/widget/widget_support.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddFood extends StatefulWidget {
   const AddFood({Key? key}) : super(key: key);
@@ -8,8 +13,204 @@ class AddFood extends StatefulWidget {
 }
 
 class _AddFoodState extends State<AddFood> {
+  final List<String> foodItems = ['Ice-cream', 'Burger', 'Salad', 'Pizza'];
+  String? value;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  TextEditingController detailsController = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
+  File? selectedImage;
+
+  Future getImage() async {
+    var image = await _picker.pickImage(source: ImageSource.gallery);
+
+    selectedImage = File(image!.path);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: const Icon(
+            Icons.arrow_back_ios_new_outlined,
+            color: Color(0xFF373866),
+          ),
+        ),
+        centerTitle: true,
+        title: Text(
+          "Add Item",
+          style: AppWidget.HeadlineTextFeildStyle(),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Upload the item picture",
+                style: AppWidget.semiBoldTextFeildStyle(),
+              ),
+              const SizedBox(height: 20.0),
+              Center(
+                child: Material(
+                  elevation: 4.0,
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 1.5),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Image.file(
+                      selectedImage!,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30.0),
+              Text(
+                "Item Name",
+                style: AppWidget.semiBoldTextFeildStyle(),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFececf8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Enter item Name",
+                    hintStyle: AppWidget.LightTextFeildStyle(),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30.0),
+              Text(
+                "Item Price",
+                style: AppWidget.semiBoldTextFeildStyle(),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFececf8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  controller: priceController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Enter item Price",
+                    hintStyle: AppWidget.LightTextFeildStyle(),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30.0),
+              Text(
+                "Item Details",
+                style: AppWidget.semiBoldTextFeildStyle(),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFececf8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  maxLines: 6,
+                  controller: detailsController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Enter item Details",
+                    hintStyle: AppWidget.LightTextFeildStyle(),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30.0),
+              Text(
+                "Item Category",
+                style: AppWidget.semiBoldTextFeildStyle(),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFececf8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    items: foodItems
+                        .map((item) => DropdownMenuItem<String>(
+                              value: item,
+                              child: Text(
+                                item,
+                                style: const TextStyle(
+                                    fontSize: 18.0, color: Colors.black),
+                              ),
+                            ))
+                        .toList(),
+                    onChanged: (value) => setState(() {
+                      this.value = value;
+                    }),
+                    dropdownColor: Colors.white,
+                    hint: const Text("Select Category"),
+                    iconSize: 36,
+                    icon: const Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.black,
+                    ),
+                    value: value,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 5.0),
+                    width: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                      child: Text(
+                        "Add",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/admin/home_admin.dart
+++ lib/admin/home_admin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodies/admin/add_food.dart';
 import 'package:foodies/widget/widget_support.dart';
 
 class HomeAdmin extends StatefulWidget {
@@ -25,28 +26,44 @@ class _HomeAdminState extends State<HomeAdmin> {
             SizedBox(
               height: 50.0,
             ),
-            Material(
-              elevation: 10.0,
-              borderRadius: BorderRadius.circular(10),
-              child: Center(
-                child: Container(
-                  padding: EdgeInsets.all(4),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(6.0),
-                        child: Image.asset(
-                          "images/food.jpg",
-                          height: 100,
-                          width: 100,
-                          fit: BoxFit.cover,
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AddFood()));
+              },
+              child: Material(
+                elevation: 10.0,
+                borderRadius: BorderRadius.circular(10),
+                child: Center(
+                  child: Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(6.0),
+                          child: Image.asset(
+                            "images/food.jpg",
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                      )
-                    ],
+                        SizedBox(
+                          width: 30.0,
+                        ),
+                        Text(
+                          "Add Food Items",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
