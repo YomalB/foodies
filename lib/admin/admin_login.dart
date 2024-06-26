def622b1f11f707aa6daecd0ffc47bd31dd8c5cd
YomalB <bashiyml10smd@gmail.com>
2024-06-26 21:44:53 +0530

    admin_log.dart

---
+++ lib/admin/admin_login.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AdminLogin extends StatefulWidget {
+  const AdminLogin({Key? key}) : super(key: key);
+
+  @override
+  State<AdminLogin> createState() => _AdminLoginState();
+}
+
+class _AdminLoginState extends State<AdminLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFededeb),
+      body: Container(
+          child: Stack(
+        children: [
+          Container(
+            margin:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height / 2),
+            padding: EdgeInsets.only(top: 45.0, left: 20.0, right: 20.0),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color.fromARGB(255, 53, 51, 51), Colors.black],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight),
+                borderRadius: BorderRadius.vertical(
+                    top: Radius.elliptical(
+                        MediaQuery.of(context).size.width, 110.0))),
+          ),
+          Container()
+        ],
+      )),
+    );
+  }
+}
